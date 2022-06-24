22a7644b8e114d6d26dcedec15121600e5bdfd4d
fedepaloz <palozzifederico@gmail.com>
2022-06-24 16:08:33 +0200

    boilerplate

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è); */
+
